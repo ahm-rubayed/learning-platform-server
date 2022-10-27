f4cb86745df4fe519e7f42f63cefa64d648499bc
ahm-rubayed <rubayedahmed04@gmail.com>
2022-10-27 12:07:32 +0600

    Rivising the code

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 
-const categories = require('./data/categories.json')
-const courses = require('./data/courses.json')
+const categories = require("./data/categories.json");
+const courses = require("./data/courses.json");
 
-app.get('/', (req, res) => {
-    res.send("Course API is running")
+app.get("/", (req, res) => {
+  res.send("Course API is running");
 });
 
-app.get('/categories', (req, res) => {
-    res.send(categories)
-})
+app.get("/categories", (req, res) => {
+  res.send(categories);
+});
 
-app.get('/courses', (req, res) => {
-    res.send(courses)
-})
+app.get("/courses", (req, res) => {
+  res.send(courses);
+});
 
-app.get('/checkout/:id', (req, res) => {
-    const id = req.params.id;
-    const course = courses.filter(n => n.courses_id == id);
-    console.log(course)
-    res.send(course)
-})
+app.get("/checkout/:id", (req, res) => {
+  const id = req.params.id;
+  const course = courses.filter((n) => n.courses_id == id);
+  console.log(course);
+  res.send(course);
+});
 
-app.get('/course/:id', (req, res) => {
-    const id = req.params.id;
-    const courseSelected = courses.filter(n => n.courses_id == id);
-    res.send(courseSelected)
-})
+app.get("/course/:id", (req, res) => {
+  const id = req.params.id;
+  const courseSelected = courses.filter((n) => n.courses_id == id);
+  res.send(courseSelected);
+});
 
 app.listen(port, () => {
-    console.log("Courseella server is running")
-})
+  console.log("Courseella server is running");
+});
